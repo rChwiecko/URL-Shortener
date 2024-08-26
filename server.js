ea6987d9ab23f731359a0165a7fbf61085fc4ab4
rChwiecko <ryanchwiecko10@gmail.com>
2024-08-26 19:31:40 -0400

    Completed functionality of tool

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortUrl')
+
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
 
@@ -15,5 +16,15 @@ app.post('/shortUrls', async (req, res) => {
    await ShortUrl.create({ full: req.body.fullUrl })
     res.redirect('/')
 })
+
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({short:req.params.shortUrl})
+    if (shortUrl == null) return res.sendStatus(404)
+    shortUrl.clicks++
+    shortUrl.save()
+    res.redirect(shortUrl.full)
+})
+
+
 /*setting default port value*/
 app.listen(process.env.PORT || 5000)
