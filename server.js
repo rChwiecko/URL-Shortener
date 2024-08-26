76c29bf6091ba46f3ff58048ebaae164ba0635c9
rChwiecko <ryanchwiecko10@gmail.com>
2024-08-26 19:11:42 -0400

    styling changes applied, basic functionality implemented

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express")
 const app = express()
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-    res.render('index.ejs')
+mongoose.connect("mongodb://localhost/urlShortener")
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render('index.ejs', {shortUrls: shortUrls})
+})
+
+app.post('/shortUrls', async (req, res) => {
+   await ShortUrl.create({ full: req.body.fullUrl })
+    res.redirect('/')
 })
 /*setting default port value*/
 app.listen(process.env.PORT || 5000)
